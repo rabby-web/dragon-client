c596be1653a3b93383f93af5c746f96946d8bbed
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-20 16:16:45 +0600

    routes setup

--- src/Routes/Routes/Routes.jsx
+++ src/Routes/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from "../../Pages/Home/Home";
+import Category from "../../Pages/Category/Category";
+import News from "../../Pages/News/News";
 
 export const routes = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/category/:id',
+                element: <Category></Category>
+            },
+            {
+                path: '/news/:id',
+                element: <News></News>
             }
         ]
     }
