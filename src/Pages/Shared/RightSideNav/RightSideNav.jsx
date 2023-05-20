1cf307c31397db1998ebc3fd1003c6e7b0e65d0f
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-20 17:57:13 +0600

    carusel added right side

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -2,6 +2,9 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaTwitch, FaLinkedin } from "react-icons/fa";
 import ListGroup from 'react-bootstrap/ListGroup';
+import Carousel from 'react-bootstrap/Carousel';
+import Brand1 from '../../../assets/carusel/daraz.png'
+import Brand2 from '../../../assets/carusel/footpanda.png'
 
 
 const RightSideNav = () => {
@@ -14,11 +17,30 @@ const RightSideNav = () => {
             <div>
                 <h5>Find Us On</h5>
                 <ListGroup>
-                    <ListGroup.Item><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
-                    <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
-                    <ListGroup.Item><FaTwitch></FaTwitch> Twitch</ListGroup.Item>
-                    <ListGroup.Item><FaLinkedin></FaLinkedin> Linkedin</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch></FaTwitch> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaLinkedin></FaLinkedin> Linkedin</ListGroup.Item>
                 </ListGroup>
+            </div>
+            <div>
+            <Carousel>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src={Brand1}
+                        alt="First slide"
+                      />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src={Brand2}
+                        alt="Second slide"
+                      />
+                    </Carousel.Item>
+                    
+                </Carousel>
             </div>
         </div>
     );
