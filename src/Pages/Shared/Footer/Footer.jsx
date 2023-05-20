594da80aa2acf9d843b08d41131dc12d740b06c9
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-20 20:29:19 +0600

    footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,7 +1,30 @@
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer</h3>
+        <div className="container">
+            <div className="row">
+                <div className="footer-col">
+                    <h4>company</h4>
+                    <ul>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="footer-col">
+                    <h4>get help</h4>
+                    <ul>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="footer-col">
+                    <h4>online news</h4>
+                    <ul>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
