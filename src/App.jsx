68992b53e0ccc9ae3f33a82191f07109d12679eb
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-20 14:37:49 +0600

    layout and routes start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { routes } from './Routes/Routes/Routes'
 
 function App() {
 
   return (
     <>
-      
+      <RouterProvider router={routes}></RouterProvider>
     </>
   )
 }
