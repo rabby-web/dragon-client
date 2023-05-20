865a236ff41410641dd7082480e21106e3fa5d67
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-20 14:16:15 +0600

    firebase bootstrap react-router-dom install and firebase config.js file added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTZ7k6iHDpp6nBZmpTCggWuHmbfI_uUdM",
+  authDomain: "dragon-news-11502.firebaseapp.com",
+  projectId: "dragon-news-11502",
+  storageBucket: "dragon-news-11502.appspot.com",
+  messagingSenderId: "251077784360",
+  appId: "1:251077784360:web:e3636334d3bcc3fc7e161d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
