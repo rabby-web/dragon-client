95accba02b3f1e45f5239f29628f623664362c8c
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-20 16:55:11 +0600

    data load from API news data

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () => {
+        fetch('http://localhost:5000/news-categories')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h2>Left Side Nav</h2>
+            <h2>All Categories: {categories.length}</h2>
+            <div>
+                {
+                    categories.map(category => <p key={category.id}> 
+                        <Link>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
