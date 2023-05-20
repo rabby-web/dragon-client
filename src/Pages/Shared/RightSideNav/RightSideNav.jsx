d97b11fe9346c2ca081f4e9c43e0e1902d736814
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-20 17:40:19 +0600

    icon added right side

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,15 +1,25 @@
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import { FaGoogle, FaGithub } from "react-icons/fa";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaTwitch, FaLinkedin } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 const RightSideNav = () => {
     return (
         <div>
-            <ButtonGroup vertical>
-                <Button variant="outline-primary"><FaGoogle></FaGoogle> Login with Google</Button>
+            <ButtonGroup className='mb-2' vertical>
+                <Button className='mb-2' variant="outline-primary"><FaGoogle></FaGoogle> Login with Google</Button>
                 <Button variant="outline-dark"><FaGithub></FaGithub> Login with GitHub</Button>
             </ButtonGroup>
+            <div>
+                <h5>Find Us On</h5>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaTwitch></FaTwitch> Twitch</ListGroup.Item>
+                    <ListGroup.Item><FaLinkedin></FaLinkedin> Linkedin</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
