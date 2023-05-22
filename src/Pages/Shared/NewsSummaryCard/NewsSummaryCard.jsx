362370fdb20917f6a27fddcbba7189bb0b6fe9d7
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-22 13:32:20 +0600

    News Summary Card

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.jsx
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.jsx
@@ -1,7 +1,20 @@
+import Card from 'react-bootstrap/Card';
+
 const NewsSummaryCard = ({news}) => {
+    const {_id, title, total_view, author, details, image_url} = news;
+    console.log(news)
     return (
         <div>
-            {news.title}
+            <Card className="text-center">
+                <Card.Header>{title}</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            </Card>
         </div>
     );
 };
