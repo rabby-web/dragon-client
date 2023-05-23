854321097f360ad71f1d35abed5f4560f9121f6f
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-23 23:35:11 +0600

    news detail added

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,11 +1,23 @@
 import { useLoaderData } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 const News = () => {
     const news = useLoaderData();
+    const {title, details, image_url} = news;
+    console.log(news)
     return (
         <div>
-            <h2>{news.title}</h2>
+           <Card>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Text>
+                    {details}
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
