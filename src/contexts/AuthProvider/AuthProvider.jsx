0de25772f0c902b9c120b7ee1fb67de7cd9d4dbd
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-27 11:54:10 +0600

    auth import

--- src/contexts/AuthProvider/AuthProvider.jsx
+++ src/contexts/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,9 @@
 import { createContext } from "react";
+import { getAuth } from 'firebase/auth'
+import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const user = {displayName: 'Z.N.R'}
